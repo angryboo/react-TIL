880067e91cce32196aeb061f5baaedd3c2f7bb3e
angryboo <buyongsong@gmail.com>
2020-05-07 12:58:18 +0900

    add: login component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
+/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import classNames from 'classnames/bind';
+import style from './App.css';
+
+const st = classNames.bind(style);
 
 class App extends Component {
-  checkLength = (receiveTitle) => {
-    let title = receiveTitle;
-    if (title.length > 0) {
-      title += ' > 0';
-    } else if (title.length === 0) {
-      title += ' === 0';
-    } else {
-      title += ' < 0';
-    }
-    return title;
+  changeInput = (text) => {
+    console.log(`사용자 ${text} 입력중`);
+  };
+
+  clickButton = () => {
+    console.log('로그인 시도');
   };
 
   render() {
-    const title = 'Hello World';
     return (
-      <div>
-        <h1>{this.checkLength(title)}</h1>
-      </div>
+      <form className={st('from')}>
+        <div className={st('text')}>ID :</div>
+        <input
+          type="text"
+          onChange={() => this.changeInput('아이디')}
+          className={st('input')}
+        />
+        <div className={st('text')}>PASS :</div>
+        <input
+          type="password"
+          onChange={() => this.changeInput('패스워드')}
+          className={st('input')}
+        />
+        <button
+          type="button"
+          onClick={this.clickButton}
+          className={st('button')}
+        >
+          로그인하기
+        </button>
+      </form>
     );
   }
 }
